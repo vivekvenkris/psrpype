2338787e39f60836d13c916af52a760e9da09105
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-10-11 18:23:37 +0200

    changed directories

--- src/constants.py
+++ src/constants.py
@@ -11,12 +11,13 @@ FILE_SIZE="file_size"
 RAW_FILE="#rawfile"
 STATUS=""
 
-PIPELINE_DIR_NAMES=["templates", "ephemeris", "flux_calibrators", "poln_calibrators", "01_rfi_excision", "02a_flatten_bandpass", 
-"02b_flux_caliibration", "03a_rfi_excision", "03b_rfi_excision", "04a_polarisation_calibration", "04b_polarisation_calibration", 
-"05a_decimation", "05b_decimation", "06a_timing", "06b_timing "]
+# PIPELINE_DIR_NAMES=["templates", "ephemeris", "flux_calibrators", "poln_calibrators", "01_rfi_excision", "02a_flatten_bandpass", 
+# "02b_flux_caliibration", "03a_rfi_excision", "03b_rfi_excision", "04a_polarisation_calibration", "04b_polarisation_calibration", 
+# "05a_decimation", "05b_decimation", "06a_timing", "06b_timing "]
 
 
-
+PIPELINE_DIR_NAMES=["templates", "ephemeris", "flux_calibrators", "poln_calibrators", "rfi_excised_L1", "flux_calibrated", 
+"rfi_excised_L2", "poln_calibrated", "decimated", "timing"]
 
 
 
