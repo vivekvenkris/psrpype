ea405f96178aa8ce42e04aedc2eb69683368d1bb
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2021-11-23 03:18:56 +1100

    filter unprocessed, optionally consolidate

--- src/process_data.py
+++ src/process_data.py
@@ -36,6 +36,7 @@ def get_args():
 	argparser.add_argument("--config", dest="config", help="config file", required=True)
 
 	argparser.add_argument("--with-slurm", dest="slurm", help="Queue with slurm", action='store_true')
+	argparser.add_argument("--consolidate", dest="consolidate", help="psradd recleaned files + produce decimated products", action='store_true')
 
 	AppUtils.add_shortlist_options(argparser)
 	Logger.add_logger_argparse_options(argparser)
@@ -54,36 +55,52 @@ def main():
 
 
 	# get the list of observations to process
-	query = db_manager.get_session().query(Observation)
+	query = db_manager.get_session().query(Observation).join(SlurmJob, Observation.slurm_id == SlurmJob.id, isouter=True)
 	query = query.filter(Observation.obs_type.in_(PULSAR_TYPES))
-	query = query.filter(Observation.processed == False)
+
+	# 1. processed is false, no slurm job attached
+	# 2. processed is false, slurm job is not running
+	# 3. processed is false, slurm job is failed
+	query = query.filter( ((Observation.processed == False) & (Observation.slurm_job == None))| 
+							((Observation.processed == False) & ((Observation.slurm_job != None) & (SlurmJob.state != 'RUNNING'))) |
+							((Observation.slurm_job != None) & 	((SlurmJob.state == 'FAILED') | (SlurmJob.state == 'OUT_OF_MEMORY'))) 
+						)
+
 	query = AppUtils.add_shortlist_filters(query, args)
 
 	observations = query.all()	
 
+	if(len(observations) == 0):
+		logger.info("No observations to process")
+		return
+
+	else:
+		logger.info("Processing {} observations".format(len(observations)))
+		for obs in observations:
+			logger.debug("Processing {} {} {}".format(obs.source, obs.obs_start_utc, obs.cfreq))
+		
 
 	slurm_launcher = SlurmLauncher(config)
 
 
 	if args.slurm: 
-		
+		job_ids = []
 		for observation in observations:
-
-			command = "python {} --config={} --obs_utcs=\"{}\" --stream_log_level=DEBUG".format(Path(__file__).resolve().as_posix(),
+			consolidate_flag= "--consolidate" if args.consolidate else ""
+			command = "python {} --config={} --obs_utcs=\"{}\" -c \"{}\" {} --stream_log_level=DEBUG".format(Path(__file__).resolve().as_posix(),
 																								Path(args.config).resolve().as_posix(), 
-							observation.obs_start_utc)
+                            observation.obs_start_utc, observation.cfreq, consolidate_flag)
 			job_id = slurm_launcher.launch(observation, command)
+			job_ids.append(job_id)
 			time.sleep(2)
 
 			slurm_job = SlurmJob(id=job_id, state="QUEUED")
-			db_manager.get_session().add(slurm_job)
 			observation.slurm_job = slurm_job
-			db_manager.get_session().add(observation)
-			db_manager.get_session().commit()
+			db_manager.add_to_db([observation,slurm_job])
 
 		logger.info("All jobs submitted..")
 
-		slurm_checker = SlurmChecker()
+		slurm_checker = SlurmChecker(job_ids)
 		SlurmChecker.signal_init()
 		slurm_checker.start()
 		slurm_checker.join()
@@ -93,9 +110,10 @@ def main():
 
 		for observation in observations:
 			observing_session = ObservingSession(config, observation)
-			observing_session.process() 
+			observing_session.process(consolidate=args.consolidate) 
 			observation.processed = True
-			db_manager.add_to_db(observation)
+		
+		db_manager.add_to_db(observations)
 
 
 
